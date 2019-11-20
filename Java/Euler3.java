664dc15c85087c6c1b704ad8a9d026eeb8a65c4c
geraldatanga44 <geraldatanga@gmail.com>
2019-11-19 21:38:03 -0800

    added euler 3 java

---
+++ Java/Euler3.java
@@ -0,0 +1,17 @@
+public class Euler3 {
+    private static int maxPrimeFactor(){
+        var value = 600851475143L;
+        var maxPrimeFac = 0;
+        for(var num = 2; num <= value / num; ++num){
+            while (value % num == 0){
+                maxPrimeFac = num;
+                value /= num;
+            }
+        }
+        return (int) value > 1 && value > maxPrimeFac ? (int) value : maxPrimeFac;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxPrimeFactor());
+    }
+}
